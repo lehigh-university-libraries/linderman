cce34cfd964f68a1afa75699f442ba80a1c6e963
Joe Corall <joe@libops.io>
2025-07-11 14:15:41 -0400

    Point to new actions-runner image location

--- scripts/maintenance/update-gha.sh
+++ scripts/maintenance/update-gha.sh
@@ -2,14 +2,6 @@
 
 set -eou pipefail
 
-# Our GitHub Actions runner docker image is maintained at https://github.com/lehigh-university-libraries/docker-builds
-# When that docker image gets updated, we want to automatically receive updates
-# we can not roll out those updates with GitHub PRs without weird timeouts happening
-# because GitHub runners container would be restarted on a rollout, causing the GitHub Action workflow to hang
-# so instead we just check for updates and handle those as they come
-RUNNER_CONTAINER="github-actions-runner"
-RUNNER_IMAGE="ghcr.io/lehigh-university-libraries/docker-builds/actions-runner:main"
-
 # helper function for docker compose commands
 docker_compose() {
     docker compose \
@@ -17,6 +9,15 @@ docker_compose() {
       -f "docker-compose.$HOSTNAME.yaml" \
       "$@"
 }
+
+
+# Our GitHub Actions runner docker image is maintained at https://github.com/lehigh-university-libraries/docker-builds
+# When that docker image gets updated, we want to automatically receive updates
+# we can not roll out those updates with GitHub PRs without weird timeouts happening
+# because GitHub runners container would be restarted on a rollout, causing the GitHub Action workflow to hang
+# so instead we just check for updates and handle those as they come
+RUNNER_CONTAINER="github-actions-runner"
+RUNNER_IMAGE=$(docker_compose config "$RUNNER_CONTAINER" --format json | jq -r '.services["'"${RUNNER_CONTAINER}"'"].image')
 
 # helper function to check there are no jobs running on our runner
 ensure_idle() {
